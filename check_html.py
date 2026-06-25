c4bab94145c57989e0360cf9f75461314e8ba873
Marko22t <marcovidaurre25@gmail.com>
2026-06-25 19:09:55 -0400

    Remove temp scripts and txt files

--- check_html.py
+++
@@ -1,5 +0,0 @@
-import re
-html = open('c:/Autodecor-web/tiendas.html', encoding='utf-8').read()
-
-sidebar_idx = html.find('<!-- SIDEBAR -->')
-print(html[sidebar_idx-1000:sidebar_idx+1000].encode('utf-8'))
--- check_layout.py
+++
@@ -1,4 +0,0 @@
-html = open('c:/Autodecor-web/tiendas.html', encoding='utf-8').read()
-idx = html.find('mapa-layout')
-open('c:/Autodecor-web/temp_layout.txt', 'w', encoding='utf-8').write(html[idx:idx+4000])
-print('done')
